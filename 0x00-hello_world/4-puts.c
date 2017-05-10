4439e4ef5592f70d2fdd1a4edf1433e578d4ac91
jeffreykanemitsu <jeffreykanemitsu@outlook.com>
2017-05-10 22:53:09 +0000

    fixed betty errors for 3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 /* Hello, puts */
 #include <stdio.h>
 
-main (void) {
-	printf("Programming is like building a multilingual puzzle\n" );
-	return 0;
-}
+main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle\n");
 
+	return (0);
+}
