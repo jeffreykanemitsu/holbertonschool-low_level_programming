9597af75fc7fa917bb0741b687d438ac08d3fd97
jeffreykanemitsu <jeffreykanemitsu@outlook.com>
2017-05-11 02:49:24 +0000

    add 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+{
+int main(void)
+	write(fd, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	return (0);
+}
