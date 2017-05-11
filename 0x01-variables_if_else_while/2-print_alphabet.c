8c4ba182f33c24189a39c3415bf38bbc055bec20
jeffreykanemitsu <jeffreykanemitsu@outlook.com>
2017-05-11 23:52:19 +0000

    added task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char az;
+
+	for (az = 'a'; az <= 'z'; az = az++)
+	{
+	putchar(az);
+	}
+	putchar('\n');
+	return (0);
+}
