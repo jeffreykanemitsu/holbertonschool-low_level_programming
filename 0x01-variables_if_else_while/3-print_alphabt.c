ef4a9aee9738e909dddbc5f4ed4a706500aed534
jeffreykanemitsu <jeffreykanemitsu@outlook.com>
2017-05-12 01:12:20 +0000

    adding task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char az;
+
+	for (az = 'a'; az <= 'z'; az++)
+	{
+	if (az != 'q' && az != 'e')
+
+		{
+
+		putchar(az)
+
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
