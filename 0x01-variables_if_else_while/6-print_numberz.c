855740c0f2c80fdb71e48cf52c010e0eaad33d6c
jeffreykanemitsu <jeffreykanemitsu@outlook.com>
2017-05-12 02:10:44 +0000

    adding task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+nclude <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
