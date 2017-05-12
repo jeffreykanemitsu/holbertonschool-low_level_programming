6497edf148f065a6a2ce6e83534984488256a4a7
jeffreykanemitsu <jeffreykanemitsu@outlook.com>
2017-05-12 01:55:15 +0000

    testing task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 9;
+
+	for (n = '0'; 9 <= '9'; n++)
+
+	putchar('\n');
+	return (0);
+}
