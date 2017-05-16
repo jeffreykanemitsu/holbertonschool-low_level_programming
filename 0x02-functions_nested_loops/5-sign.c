93ecb446662bd5de580c21cb88a0364f2a785b0c
jeffreykanemitsu <jeffreykanemitsu@outlook.com>
2017-05-16 22:08:37 +0000

    task 5 adding

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * print_sign - check the code for Holberton School students.
+ * @n: variable being tested.
+ * Return: Always 0.
+ */
+int print_sign(int n)
+{
+	int result;
+
+	if (n > 0)
+	{
+		result = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		result = 0;
+		_putchar(0);
+	}
+	else if (n < 0)
+	{
+		result = -1;
+		_putchar('-');
+	}
+	return (result);
+}
