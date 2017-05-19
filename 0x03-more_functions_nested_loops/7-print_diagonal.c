70ec39df15747c6da90987c36b31ab42a5f70d53
jeffreykanemitsu <jeffreykanemitsu@outlook.com>
2017-05-19 05:23:30 +0000

    task 8: adding

---
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * print_diagonal - check the code for Holberton School students.
+ * @n: variable being tested.
+ *
+ */
+void print_diagonal(int n)
+{
+	int a;
+	int b;
+
+	for (b = 0; b < a; b++)
+	{
+		for (a = 0; a < n; a++)
+		{
