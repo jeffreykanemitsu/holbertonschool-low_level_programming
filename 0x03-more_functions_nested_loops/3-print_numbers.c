453d23e7d914db1bf5184a66236a89e80e4a2253
jeffreykanemitsu <jeffreykanemitsu@outlook.com>
2017-05-19 01:12:05 +0000

    task 3: testing

---
+++ 0x03-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_numbers - check the code for Holberton School students.
+ *
+ *
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
