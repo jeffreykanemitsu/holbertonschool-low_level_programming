d4fe9372f56a07c1901b47d26054885ddfe17b02
jeffreykanemitsu <jeffreykanemitsu@outlook.com>
2017-05-20 00:05:19 +0000

    task 9: removed white spaces, cleaned up code

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	int n;
 
-	for (n = 1; n <= 100 ; n++)
+	for (n = 1; n <= 99 ; n++)
 	{
 		if (n % 3 == 0 && n % 5 == 0)
 		{
@@ -22,14 +22,14 @@ int main(void)
 		{
 			printf("Fizz ");
 		}
-		else if (n == 100)
-		{
-			printf("Buzz");
-		}
 		else
 		{
 			printf("%d ", n);
 		}
+	}
+	if (n == 100)
+	{
+		printf("Buzz\n");
 	}
 	return (0);
 }
