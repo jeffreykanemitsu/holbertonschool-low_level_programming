aa2678c514807471527b61a252412bd354c39410
jeffreykanemitsu <jeffreykanemitsu@outlook.com>
2017-05-23 19:08:45 +0000

    task 3: fixed betty error

--- 0x04-pointers_arrays_strings/3-puts.c
+++ 0x04-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 /**
  * _puts - prints the string
- *
+ * @str: string
  * Return - void
  */
 void _puts(char *str)
 {
 	int a = 0;
 
-	while(str[a])
+	while (str[a])
 	{
 		_putchar(str[a]);
 		a++;
