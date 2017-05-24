df57a6b97dcedbf521b6cc33cda6858f715faee9
jeffreykanemitsu <jeffreykanemitsu@outlook.com>
2017-05-24 18:18:18 +0000

    task 9: added & holberton.h: added

---
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * _strcpy - copies the string pointed to by src
+ * @src: source
+ * @dest: destination
+ *
+ * Return: value of pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+	{
+		while (src[i] != '\0')
+		{
+			dest[i] = src[i];
+			i++;
+		}
+	}
+	return (dest);
+}
--- 0x04-pointers_arrays_strings/holberton.h
+++ 0x04-pointers_arrays_strings/holberton.h
@@ -28,3 +28,4 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
