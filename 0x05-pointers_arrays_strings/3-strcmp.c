aeecd358b7583c03ad16935929310bc3ec0333cc
jeffreykanemitsu <jeffreykanemitsu@outlook.com>
2017-05-26 04:45:26 +0000

    task 3: added. holberton.h: updated

---
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/* _strcmp - compare two strings
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: Always 0.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int x;
+
+	for (x = 0; s1[x] != '\0'; x++)
+	{
+		if (s1[x] != s2[x])
+		{
+			return (s1[x] - s2[x]);
+		}
+	}
+	return (0);
+}
