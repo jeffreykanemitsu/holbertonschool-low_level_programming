5ba9f549e872382dc2690fa9d649920981eb7d03
jeffreykanemitsu <jeffreykanemitsu@outlook.com>
2017-05-26 23:58:37 +0000

    task 8: added. holberton.h: updated

---
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * rot13 - using rot13 to encode a string
+ * @s: character being changed
+ * Return: Always 0.
+ */
+char *rot13(char *s)
+{
+	char in[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char out[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	
+	int a;
+	int b;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for (b = 0; in[b]; b++)
+		{
+			if (s[a] == in[b])
+			{
+				s[a] = out[b];
+				break;
+			}
+		}
+	}
+	return (s);
+}
+
+	
