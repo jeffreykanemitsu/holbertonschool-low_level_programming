15aba8105c9b286ccf1a0f29d2d2574a525eb26e
jeffreykanemitsu <jeffreykanemitsu@outlook.com>
2017-06-02 19:01:14 +0000

    task 5: added

---
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+/**
+ * NaN - natural number
+ * @a: variable being tested
+ * @b: variable being tested
+ * Return: square root or -1
+ */
+int NaN(int a, int b)
+{
+	int c = b * b;
+
+	if (c > a)
+	{
+		return (-1);
+	}
+	else if ( c == a)
+	{
+		return (b);
+	}
+	else
+	{
+		return (NaN(a, b + 1));
+	}
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: natural square root
+ * Return: square root or -1
+ */
+int _sqrt_recursion(int n)
+{
+	return (NaN(n, 1));
+}
