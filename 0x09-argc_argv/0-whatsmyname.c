d3a59234d7c3959ebf1408705e296f6518bcc2ac
jeffreykanemitsu <jeffreykanemitsu@outlook.com>
2017-06-05 06:12:54 +0000

    task 0: added

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints name, followed by a new line
+ * @argc: number of arguments
+ * @argv: holds string value
+ * 
+ * Return: 0
+ */
+int main(int argc __attribute__ ((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
