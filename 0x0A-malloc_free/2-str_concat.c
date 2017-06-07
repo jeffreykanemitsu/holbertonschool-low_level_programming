ee75e85fc11af2ea0a64065de525f0739efa8ad6
jeffreykanemitsu <jeffreykanemitsu@outlook.com>
2017-06-07 07:55:13 +0000

    task 2: separated the if statement for s1 == null and s2 == null

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -15,9 +15,12 @@ char *str_concat(char *s1, char *s2)
 	int length1 = 0;
 	int length2 = 0;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
 		s1 = "";
+	}
+	if (s2 == NULL)
+	{
 		s2 = "";
 	}
 	for (x = 0; s1[x]; x++)
