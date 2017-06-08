912235ac8a62e29c9bf8e506662c0d8ae308dffb
jeffreykanemitsu <jeffreykanemitsu@outlook.com>
2017-06-08 11:16:05 +0000

    task 1: unfinished

---
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * @n: unsigned int
+ *
+ * Return: newly allocated space in memory
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
