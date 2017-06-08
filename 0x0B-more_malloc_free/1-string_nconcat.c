e086cf92e9d5a5f0926b307863617140eb368e09
jeffreykanemitsu <jeffreykanemitsu@outlook.com>
2017-06-08 23:50:14 +0000

    task 1: added

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -10,3 +10,40 @@
  * Return: newly allocated space in memory
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int x;
+	unsigned int y;
+	unsigned int z;
+	char *con;
+
+	if (s1 == 0)
+	{
+		return ("");
+	}
+	if (s2 == 0)
+	{
+		return ("");
+	}
+	for (x = 0; s1[x]; x++)
+	{
+		;
+	}
+	for (y = 0; s2[y]; y++)
+	{
+		;
+	}
+	con = malloc((x + y + 1) * sizeof(char));
+	if (con == NULL)
+	{
+		return (NULL);
+	}
+	for (z = 0; z < x; z++)
+	{
+		con[z] = s1[z];
+	}
+	for (y = 0; y < n; y++, z++)
+	{
+		con[z] = s2[y];
+	}
+	return (con);
+}
