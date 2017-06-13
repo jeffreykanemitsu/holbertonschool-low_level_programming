d3057015bfacae69da40554bb99a269a509259ac
jeffreykanemitsu <jeffreykanemitsu@outlook.com>
2017-06-13 01:27:09 +0000

    task 0: added

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure of dog
+ * @name: char string for name of dog
+ * @age: float string for age of dog
+ * @owner: char string for owner of dog
+ *
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
