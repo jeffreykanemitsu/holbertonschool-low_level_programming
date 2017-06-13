046ba6ed9e70915287e7f34dc61162e16943ab1d
jeffreykanemitsu <jeffreykanemitsu@outlook.com>
2017-06-13 04:18:47 +0000

    dog.h: added new type dog_t for type struct dog

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -14,7 +14,7 @@ typedef struct dog
 	char *owner;
 } my_dog;
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
-
-
+typedef struct dog dog_t;
 #endif
