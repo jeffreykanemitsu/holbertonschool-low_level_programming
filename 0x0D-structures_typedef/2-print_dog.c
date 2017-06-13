9384cd3191962d70151550fe2f941a9dca5077c6
jeffreykanemitsu <jeffreykanemitsu@outlook.com>
2017-06-13 03:55:36 +0000

    task 2: started task 2 over because checker errors wouldn't change even with correct output

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -8,10 +8,26 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d != NULL)
+	if (d == NULL)
 	{
-		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
-		printf("Age: %f\n", d->age);
-		printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
+		return;
+	}
+	if (d->name != NULL)
+	{
+		printf("Name: %s\n", d->name);
+	}
+	else
+	{
+		printf("Name: (nil)\n");
+	}
+	printf("age: %f\n", d->age);
+	
+	if (d->owner != NULL)
+	{
+		printf("Owner: %s\n", d->owner);
+	}
+	else
+	{
+		printf("Owner: (nil)\n");
 	}
 }
