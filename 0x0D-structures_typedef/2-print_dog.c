8b8847e8d7346a0eecd3285d133753f21e976214
jeffreykanemitsu <jeffreykanemitsu@outlook.com>
2017-06-13 04:12:00 +0000

    task 2: rewrote code to see if checker will accept it. correct output when script is ran but still all checkers marked wrong

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -1,35 +1,17 @@
 #include "dog.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * print_dog - prints a struct dog
+ * print_dog - prints struct dog
  * @d: pointer to structure
- *
  * Return: void
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
+	if (d)
 	{
-		return;
-	}
-	if (d->name != NULL)
-	{
-		printf("Name: %s\n", d->name);
-	}
-	else
-	{
-		printf("Name: (nil)\n");
-	}
-	if (d->name != NULL)
-	{
-		printf("age: %f\n", d->age);
-	}
-	if (d->owner != NULL)
-	{
-		printf("Owner: %s\n", d->owner);
-	}
-	else
-	{
-		printf("Owner: (nil)\n");
+		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
 	}
 }
