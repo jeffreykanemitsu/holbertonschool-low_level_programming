0ff239ae93c496534f2e9149c5b91c95388e937d
jeffreykanemitsu <jeffreykanemitsu@outlook.com>
2017-06-16 23:03:29 +0000

    task 2: added

---
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -0,0 +1,35 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * print_strings - prints multiple strings
+ * @separator: string printed between the strings
+ * @n: number of strings passed to function
+ *
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int x;
+	char *y;
+	va_list valist;
+
+	va_start(valist, n);
+	for (x = 0; x < n; x++)
+	{
+		y = va_arg(valist, char *);
+		if (y == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", y);
+		}
+		if (separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(valist);
+}
