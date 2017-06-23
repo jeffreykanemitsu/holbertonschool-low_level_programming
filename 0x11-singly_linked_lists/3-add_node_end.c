0eef364c43ac8dc6e976cbbe8c5a09a4a4cb80a7
jeffreykanemitsu <jeffreykanemitsu@outlook.com>
2017-06-23 21:30:46 +0000

    task 3: added

--- 0x11-singly_linked_lists/3-add_node_end.c
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -8,4 +8,37 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	
+	list_t *new_node;
+	list_t *node;
+	int x;
+
+	for (x = 0; str[x]; x++)
+	{
+		;
+	}
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->str = strdup(str);
+	new_node->len = x;
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		node = *head;
+		while (node->next != NULL)
+		{
+			node = node->next;
+		}
+		node->next = new_node;
+	}
+	return (new_node);
+}
