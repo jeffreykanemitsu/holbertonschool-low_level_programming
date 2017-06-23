d640314d5330f22a7d6b7040a43c3fbb374ee86b
jeffreykanemitsu <jeffreykanemitsu@outlook.com>
2017-06-23 07:05:36 +0000

    task 3: added but unfinished

---
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * add_node_end - adds a new node at the end of list
+ * @head: head of list
+ * @str: string
+ *
+ * Return: address of the new element, or null if fialed
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	
