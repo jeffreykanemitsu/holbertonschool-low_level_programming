e8850e2aa90f4c4947f9e50f8179467228337a19
jeffreykanemitsu <jeffreykanemitsu@outlook.com>
2017-06-29 01:26:59 +0000

    task 9: fixed betty errors

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,29 +8,29 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-        unsigned int count;
-        listint_t *current_node;
+	unsigned int count;
+	listint_t *current_node;
 
-        current_node = head;
-        for (count = 0; count < index; count++)
-        {
-                if (current_node != NULL)
-                {
-                        current_node = current_node->next;
-                }
-                else
-                {
-                        return (NULL);
-                }
-        }
-        if (current_node != NULL)
-        {
-                return (current_node);
-        }
-        else
-        {
-                return (NULL);
-        }
+	current_node = head;
+	for (count = 0; count < index; count++)
+	{
+		if (current_node != NULL)
+		{
+			current_node = current_node->next;
+		}
+		else
+		{
+			return (NULL);
+		}
+	}
+	if (current_node != NULL)
+	{
+		return (current_node);
+	}
+	else
+	{
+		return (NULL);
+	}
 }
 /**
  * add_nodeint - adds a new node at beginning of linked list
@@ -40,17 +40,17 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-        listint_t *new_node;
+	listint_t *new_node;
 
-        new_node = malloc(sizeof(listint_t));
-        if (new_node == NULL)
-        {
-                return (NULL);
-        }
-        new_node->n = n;
-        new_node->next = *head;
-        *head = new_node;
-        return (new_node);
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
 }
 /**
  * insert_nodeint_at_index - inserts a new node at given position
@@ -62,28 +62,28 @@ listint_t *add_nodeint(listint_t **head, const int n)
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-        listint_t *new_node;
-        listint_t *temp;
-        listint_t *pnode;
+	listint_t *new_node;
+	listint_t *temp;
+	listint_t *pnode;
 
-        if (head == NULL)
-        {
-                return (NULL);
-        }
-        new_node = malloc(sizeof(listint_t));
-        if (new_node == NULL)
-        {
-                return (NULL);
-        }
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
 	if (idx == 0)
 	{
 		return (NULL);
 	}
-        pnode = get_nodeint_at_index(*head, idx - 1);
-        pnode = new_node->next;
-        temp = pnode->next;
-        pnode->next = new_node;
+	pnode = get_nodeint_at_index(*head, idx - 1);
+	pnode = new_node->next;
+	temp = pnode->next;
+	pnode->next = new_node;
 	pnode->n = n;
-        new_node = temp;
-        return (new_node);
+	new_node = temp;
+	return (new_node);
 }
