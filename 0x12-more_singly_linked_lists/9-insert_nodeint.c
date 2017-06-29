14d000fa16559404ca1bec6dfe2e2ed1a9594329
jeffreykanemitsu <jeffreykanemitsu@outlook.com>
2017-06-29 06:20:42 +0000

    Task 9: removed white space

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -99,16 +99,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new_node = add_nodeint_end(head, n);
 		return (new_node);
 	}
-	else
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
 	{
-		new_node = malloc(sizeof(listint_t));
-		if (new_node == NULL)
-		{
-			return (NULL);
-		}
-		pnode = get_nodeint_at_index(*head, idx - 1);
-		new_node->next = pnode->next;
-		new_node->n = n;
-		return (new_node);
+		return (NULL);
 	}
+	pnode = get_nodeint_at_index(*head, idx - 1);
+	new_node->next = pnode->next;
+	new_node->n = n;
+	return (new_node);
 }
