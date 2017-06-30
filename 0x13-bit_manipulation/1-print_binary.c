99910fd61a3d319525e402054b71f87b082fc3c0
jeffreykanemitsu <jeffreykanemitsu@outlook.com>
2017-06-30 05:45:44 +0000

    task 1: unfinished

---
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_binary - prints binary representation of num
+ * @n: value to print
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	else
+	{
+		
