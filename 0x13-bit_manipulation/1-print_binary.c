827494931decdccfdb74c6b4daf102fc21639712
jeffreykanemitsu <jeffreykanemitsu@outlook.com>
2017-07-01 00:44:10 +0000

    task 1: added

--- 0x13-bit_manipulation/1-print_binary.c
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -7,10 +7,33 @@
  */
 void print_binary(unsigned long int n)
 {
+	unsigned long int x;
+	unsigned long int r;
+	unsigned long int l;
+	short int switch_on = 0;
+	unsigned long int size;
+
+	size = sizeof(unsigned long int) * 8;
 	if (n == 0)
 	{
 		_putchar('0');
 	}
 	else
 	{
-		
+		for (x = 0; x < size; x++)
+		{
+			l = n << 1;
+			r = l >> 1;
+			if (n != r)
+			{
+				switch_on = 1;
+				_putchar('1');
+			}
+			else if (switch_on == 1)
+			{
+				_putchar('0');
+			}
+			n = n << 1;
+		}		
+	}
+}
