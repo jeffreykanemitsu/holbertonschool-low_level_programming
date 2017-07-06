51dc83a2b50842c60bf5acf50606fdd809f8071b
jeffreykanemitsu <jeffreykanemitsu@outlook.com>
2017-07-06 20:41:55 +0000

    task 1: removed betty error: whitespaces

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -23,10 +23,9 @@ int create_file(const char *filename, char *text_content)
 	}
 	for (len = 0; text_content && text_content[len] != '\0'; len++)
 		;
-	if (text_content && len)	
+	if (text_content && len)
 	{
 		file = write(fd, text_content, len);
-		
 	}
 	close(fd);
 	{
