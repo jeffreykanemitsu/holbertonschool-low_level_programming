c78a3a732171c5cb83309bb00f63a7d9777a8e80
jeffreykanemitsu <jeffreykanemitsu@outlook.com>
2017-07-06 20:23:35 +0000

    task 1: added

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ * create_file - creates file filename
+ * @filename: name of file to create
+ * @text_content: a NULL terminated strings to write to file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, x, file;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (text_content)
+	{
+		for (x = 0; text_content[x] != '\0'; x++)
+		{
+			file = write(fd, text_content, x);
+		}
+	}
+	close(fd);
+	{
+		if (file == -1)
+		{
+			return (-1);
+		}
+	}
+	return (1);
+}
