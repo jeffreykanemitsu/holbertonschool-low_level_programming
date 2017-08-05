e79b3cb5bb4350bcd8a52329876fee14f2907168
jeffreykanemitsu <jeffreykanemitsu@gmail.com>
2017-08-05 23:34:35 +0000

    task 4: added description

--- 0x16-doubly_linked_lists/4-free_dlistint.c
+++ 0x16-doubly_linked_lists/4-free_dlistint.c
@@ -1,5 +1,9 @@
 #include "lists.h"
-
+/**
+ * free_dlistint - frees a linked list
+ * @head: points to head of list
+ * Return: none
+ */
 void free_dlistint(dlistint_t *head)
 {
 	if (head == NULL)
