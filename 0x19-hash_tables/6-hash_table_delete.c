080fa462e57efa80912f4b7f146a6776b41542c3
jeffreykanemitsu <jeffreykanemitsu@gmail.com>
2017-09-09 04:35:33 +0000

    task 6: added

---
+++ 0x19-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table that is going to be deleted
+ * Return: void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	usnigned int i;
+	hash_node_t *next_node;
+
+	if (ht == NULL || ht->size == 0 || ht->array == NULL)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		for (; ht->array[i]; ht->array[i] = next_node)
+		{
+			next_node = ht->array[i]->next;
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
+			free(ht->array[i]);
+		}
+		if (ht->array[i] == NULL)
+			free(ht->array[i]);
+	}
+	free(ht->array);
+	free(ht);
+}
