576f5177a9cfcbe6013cf92f69a5308cf9eafea6
jeffreykanemitsu <jeffreykanemitsu@gmail.com>
2017-10-28 06:24:36 +0000

    binary_tree_print.c, binary_trees.h, and task 10-13: added

---
+++ 0x1C-binary_trees/13-binary_tree_nodes.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
+ * @tree: pointer to the root node of the tree to be measured
+ * Return: node count
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+		return (0);
+	else
+		return (1 + binary_tree_nodes(tree->right) + binary_tree_nodes(tree->left));
+}	
