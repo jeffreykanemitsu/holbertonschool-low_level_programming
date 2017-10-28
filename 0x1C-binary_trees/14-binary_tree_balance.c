ea5a6db7959c1b7e343065e8da835f4385223f98
jeffreykanemitsu <jeffreykanemitsu@gmail.com>
2017-10-28 06:27:03 +0000

    task 14: getting 2 wrong in output. sadface

---
+++ 0x1C-binary_trees/14-binary_tree_balance.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to be measured
+ * Return: tree height or 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left;
+	size_t right;
+
+	if (tree == NULL || ((tree->left == NULL && tree->right == NULL)))
+		return (0);
+	left = binary_tree_height(tree->left) + 1;
+	right = binary_tree_height(tree->right) + 1;
+	if (left > right)
+		return (left);
+	else
+		return (right);
+}
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: pointer to the root node of the tree to be measured
+ * Return: tree height or 0 if tree is NULL
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left, right;
+
+	if (tree == NULL)
+		return (0);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	return (left - right);
+}	
